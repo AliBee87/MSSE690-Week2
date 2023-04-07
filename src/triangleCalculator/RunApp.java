3324f1f7931ca645a68e9b3d8f719e95c5edb45f
Ali <alyssa.benson.wsu@gmail.com>
2023-04-07 08:30:51 -0600

    Added menu, added prime number method, added method for determining triangle type using three sides back

--- src/triangleCalculator/RunApp.java
+++ src/triangleCalculator/RunApp.java
@@ -1,29 +1,84 @@
 package triangleCalculator;
 
-import java.text.NumberFormat;
-import java.util.InputMismatchException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class RunApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TriangleCalculator triangleCalculator = new TriangleCalculator();
         //create Scanner object
         Scanner scanner = new Scanner(System.in);
-        //ask for user input to determine area
-        System.out.println("Enter the base of the triangle");
-        double sideA = scanner.nextDouble();
-        System.out.println("Enter the height of the triangle");
-        double sideB = scanner.nextDouble();
-        //call calculateArea
-        triangleCalculator.calculateArea(sideA, sideB);
-        //ask for user input to determine triangle type
-        System.out.println("Now, let's determine if your triangle is an equilateral, isosceles, or scalene triangle. Please enter the length of the first side of the triangle: ");
-        double sideOne = scanner.nextDouble();
-        System.out.println("Enter the second length of the second side: ");
-        double sideTwo = scanner.nextDouble();
-        System.out.println("Enter the length of the third side: ");
-        double sideThree = scanner.nextDouble();
-        //call determineTriangleType method
-        triangleCalculator.determineTriangleType(sideOne, sideTwo, sideThree);
+        String choice;
+        System.out.println("\nWelcome to Ali's Super Fancy Calculator! \nHere are the ultra sophisticated operations:");
+
+
+        while(true){
+            String menu = """
+                    \n\t1 Calculate area of triangle using its base and height
+                    \t2 Calculate area of triangle using three sides
+                    \t3 Determine what kind of triangle you have
+                    \t4 Review prime numbers from 1-100
+                    \t5 Exit the Calculator
+                        \nMake your selection!                
+                    """;
+            System.out.println(menu);
+            choice = scanner.next();
+
+            switch(choice){
+                case "1" :
+                    //ask for user input to determine area
+                    System.out.println("Enter the base of the triangle");
+                    double base = scanner.nextDouble();
+                    System.out.println("Enter the height of the triangle");
+                    double height = scanner.nextDouble();
+                    //call calculateArea
+                    triangleCalculator.calculateArea(base, height);
+                    break;
+                case "2" :
+                    //ask for user input to determine area with three sides
+                    System.out.println("Enter all the sides of the triangle");
+                    System.out.println("Side A: ");
+                    double sideA = scanner.nextDouble();
+                    System.out.println("Side B: ");
+                    double sideB = scanner.nextDouble();
+                    System.out.println("Side C: ");
+                    double sideC = scanner.nextDouble();
+                    //call calculateArea method that uses three sides
+                    triangleCalculator.calculateAreaWithSides(sideA, sideB, sideC);
+                    break;
+                case "3" :
+                    //ask for user input to determine triangle type using three sides
+                    System.out.println("Let's determine if your triangle is an equilateral, isosceles, or scalene triangle. Please enter the length of the first side of the triangle: ");
+                    double sideOne = scanner.nextDouble();
+                    System.out.println("Enter the second length of the second side: ");
+                    double sideTwo = scanner.nextDouble();
+                    System.out.println("Enter the length of the third side: ");
+                    double sideThree = scanner.nextDouble();
+                    //call determineTriangleType method
+                    triangleCalculator.determineTriangleType(sideOne, sideTwo, sideThree);
+                    break;
+                case "4" :
+                    System.out.println("It's Prime Time!");
+                    triangleCalculator.primeNumberCalculator();
+                    break;
+                case "5" :
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                default :
+                    System.out.println("Invalid selection. Please try again!");
+                    break;
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
     }
 }
--- src/triangleCalculator/TriangleCalculator.java
+++ src/triangleCalculator/TriangleCalculator.java
@@ -1,15 +1,19 @@
 package triangleCalculator;
 
 import java.util.Objects; //IntelliJ is telling me this is necessary for hash
+import java.util.Scanner;
 
 public class TriangleCalculator {
     private double base;
     private double height;
+    private double sideOne;
+    private double sideTwo;
+    private double sideThree;
     private double sideA;
     private double sideB;
     private double sideC;
-
     enum Type {EQUILATERAL, ISOSCELES, SCALENE, NOTTRIANGLE}
+    Scanner scanner = new Scanner(System.in);
 
     //no arg constructor
     public TriangleCalculator() {
@@ -19,7 +23,6 @@ public class TriangleCalculator {
     public TriangleCalculator(double base, double height) {
         this.base = base;
         this.height = height;
-
     }
 
     //constructor for determineTriangleType
@@ -31,17 +34,28 @@ public class TriangleCalculator {
 
     //calculate area of triangle
     public void calculateArea(double base, double height) {
-        double answer;
         if (base > 0 && height > 0) {
-            answer = base * height / 2;
-            System.out.println("The area of the triangle is: " + answer);
+            double answer = base * height / 2;
+            System.out.println("The area of the triangle is "+ answer);
         } else
             System.out.println("Invalid entry. Please enter an integer greater than 0.");
     }
 
+   //added area calculation with three sides back
+    public void calculateAreaWithSides(double sideOne, double sideTwo, double sideThree){
+        if ((sideOne + sideTwo) > sideThree && (sideOne + sideThree) > sideTwo && (sideTwo + sideThree) > sideOne) {
+            double s = (sideOne + sideTwo + sideThree) / 2;
+            double area = Math.sqrt(s * (s - sideOne) * (s - sideTwo) * (s- sideThree));
+            System.out.println("The area of the triangle is: " + area);
+        }
+        else {
+            System.out.println("The triangle with entered dimensions does not exist.");
+        }
+
+    }
     //determine if isosceles, scalene, or equilateral
     public void determineTriangleType(double sideA, double sideB, double sideC) {
-        Type currentType = Type.EQUILATERAL;
+           Type currentType = Type.EQUILATERAL;
         if ((sideA + sideB <= sideC) || (sideA + sideC <= sideB) || (sideB + sideC <= sideA)) {
             currentType = Type.NOTTRIANGLE;
         } else if ((sideA == sideB) || (sideB == sideC) || (sideC == sideA)) {
@@ -52,22 +66,56 @@ public class TriangleCalculator {
             currentType = Type.SCALENE;
         }
         switch (currentType) {
-            case NOTTRIANGLE -> {
+            case NOTTRIANGLE : {
                 System.out.println("This is not a triangle.");
                 break;
             }
-            case ISOSCELES -> {
+            case ISOSCELES : {
                 System.out.println("This is an isosceles triangle.");
                 break;
             }
-            case EQUILATERAL -> {
-                System.out.println("This is an equilateral triangle");
+            case EQUILATERAL : {
+                System.out.println("This is an equilateral triangle.");
                 break;
             }
-            case SCALENE -> {
-                System.out.println("This is a scalene triangle");
+            case SCALENE : {
+                System.out.println("This is a scalene triangle.");
                 break;
             }
+
+        }
+
+    }
+    //keeping this method because it makes more sense to me
+    public void determineTriangleTypeTwo(double sideA, double sideB, double sideC){
+        if ((sideA + sideB <= sideC) || (sideA + sideC <= sideB) || (sideB + sideC <= sideA)) {
+            System.out.println("This is not a triangle.");
+        } else if ((sideA == sideB) || (sideB == sideC) || (sideC == sideA)) {
+            System.out.println("this is an isosceles triangle.");
+        } else if ((sideA == sideB) && (sideB == sideC) && (sideC == sideA)) {
+            System.out.println("This is an equilateral triangle.");
+        } else {
+            System.out.println("This is a scalene triangle");
+        }
+    }
+
+    public void primeNumberCalculator(){
+        int i, j, isPrime, n;
+        System.out.println("\nAll prime numbers between 1 and 100: ");
+        //for every number between 2 and 100, check whether prime number or not
+        for (i = 2; i <= 100; i++){
+            isPrime = 0;
+            //check whether i is prime or not
+            for (j = 2; j <= i/2; j++){
+                //if any number between 2 to i/2 divides i completely, it cannot be a prime number
+                if (i % j == 0) {
+                    isPrime = 1;
+                    break;
+                }
+            }
+            if (isPrime == 0)
+                System.out.print(i + " ");
+
 
         }
 
@@ -91,25 +139,56 @@ public class TriangleCalculator {
     public void setSideC(double sideC) {
         this.sideC = sideC;
     }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getSideOne() {
+        return sideOne;
+    }
+
+    public double getSideTwo() {
+        return sideTwo;
+    }
+
+    public double getSideThree() {
+        return sideThree;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TriangleCalculator that = (TriangleCalculator) o;
-        return Double.compare(that.base, base) == 0 && Double.compare(that.height, height) == 0 && Double.compare(that.sideA, sideA) == 0 && Double.compare(that.sideB, sideB) == 0 && Double.compare(that.sideC, sideC) == 0;
+        return Double.compare(that.base, base) == 0 && Double.compare(that.height, height) == 0 && Double.compare(that.sideOne, sideOne) == 0 && Double.compare(that.sideTwo, sideTwo) == 0 && Double.compare(that.sideThree, sideThree) == 0 && Double.compare(that.sideA, sideA) == 0 && Double.compare(that.sideB, sideB) == 0 && Double.compare(that.sideC, sideC) == 0 && Objects.equals(scanner, that.scanner);
     }
+
     @Override
     public int hashCode() {
-        return Objects.hash(base, height, sideA, sideB, sideC);
+        return Objects.hash(base, height, sideOne, sideTwo, sideThree, sideA, sideB, sideC, scanner);
     }
+
     @Override
     public String toString() {
         return "TriangleCalculator{" +
                 "base=" + base +
                 ", height=" + height +
+                ", sideOne=" + sideOne +
+                ", sideTwo=" + sideTwo +
+                ", sideThree=" + sideThree +
                 ", sideA=" + sideA +
                 ", sideB=" + sideB +
                 ", sideC=" + sideC +
+                ", scanner=" + scanner +
                 '}';
     }
 }
